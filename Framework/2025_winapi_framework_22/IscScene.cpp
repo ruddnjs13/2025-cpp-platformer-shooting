8a9e6a49a07527c1c716fe7859f65271862320f6
Candy <dltkr0424@gmail.com>
2025-11-24 20:19:52 +0900

    test: IscSecene

--- Framework/2025_winapi_framework_22/IscScene.cpp
+++ Framework/2025_winapi_framework_22/IscScene.cpp
@@ -1,8 +1,11 @@
 #include "pch.h"
 #include "IscScene.h"
+#include "Object.h"
+#include "Player.h"
 
 void IscScene::Init()
 {
+	Spawn<Player>(Layer::PLAYER, { 300, 300 }, { 10, 10 });
 }
 
 void IscScene::Update()
